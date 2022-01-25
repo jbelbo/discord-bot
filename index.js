0b3e5949007842b839b0ce2bb16ac8fae53f1c89
matias <matias@hipecommerce.com>
2022-01-25 18:46:37 -0300

    wip: anrabot poc

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const { Client, Intents } = require('discord.js')
+const axios = require('axios')
+
+const bot = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+})
+const targets = ['AnRa', 'CEO', 'Tony']
+const responses = ['No seas pelotudo']
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}`)
+})
+
+bot.on('messageCreate', (msg) => {
+  if (msg.author.bot) return
+
+  const foundWord = targets.find((target) => msg.content.includes(target))
+  if (foundWord) {
+    msg.channel.send(`${responses[0]}`)
+  }
+})
+
+bot.login(process.env.TOKEN)
